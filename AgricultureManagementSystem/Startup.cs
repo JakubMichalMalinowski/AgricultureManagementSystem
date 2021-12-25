92b3db6f9fa6c8ff5dd1280b66ea239eea3f6e40
JakubMichalMalinowski <jakumal648@student.polsl.pl>
2021-12-25 02:19:54 +0100

    remove asp.net identity and change start screen greeting

--- AgricultureManagementSystem/Startup.cs
+++ AgricultureManagementSystem/Startup.cs
@@ -25,8 +25,6 @@ namespace AgricultureManagementSystem
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages()
                 .AddRazorRuntimeCompilation();
