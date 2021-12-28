ca94578ff26066d771ecdcd807d2c67944bb49ad
JakubMichalMalinowski <jakumal648@student.polsl.pl>
2021-12-28 14:20:04 +0100

    change typeofwork to typeofmachine

--- AgricultureManagementSystem/Models/Machine.cs
+++ AgricultureManagementSystem/Models/Machine.cs
@@ -10,9 +10,9 @@ namespace AgricultureManagementSystem.Models
         [Range(0.1, 30, ErrorMessage = "Szerokość robocza musi mieścić się w przzedziale od 0.1 do 30")]
         public float WorkingWidth { get; set; }
 
-        [Required(ErrorMessage = "Podaj sposób pracy maszyną")]
-        [Display(Name = "Wykonywana praca maszyny")]
-        public string TypeOfWork { get; set; }
+        [Required(ErrorMessage = "Podaj typ maszyny")]
+        [Display(Name = "Typ maszyny")]
+        public string TypeOfMachine { get; set; }
 
         [Display(Name = "Wymagana moc pociągowa (KM)")]
         [Range(1, 2000, ErrorMessage = "Moc potrzebna do pracy z maszyną musi mieścić się w przedziale od 1 do 2000")]
